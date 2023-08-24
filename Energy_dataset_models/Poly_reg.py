28414d6743303f0956548efe1a96d7c34b40237d
AhzyUmair <98019935+AhzyUmair@users.noreply.github.com>
2023-08-25 02:53:30 +0530

    Create Poly_reg.py

---
+++ Energy_dataset_models/Poly_reg.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Real_data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+poly_reg = PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X_train)
+regressor = LinearRegression()
+regressor.fit(X_poly, y_train)
+
+y_pred = regressor.predict(poly_reg.transform(X_test))
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+from sklearn.metrics import r2_score
+r2_score(y_test, y_pred)
