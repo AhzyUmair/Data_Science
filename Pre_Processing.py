3f58593fa24e9e710fdfff5834270459527df10c
AhzyUmair <98019935+AhzyUmair@users.noreply.github.com>
2023-08-26 18:00:11 +0530

    Update Pre_Processing.py

--- Pre_Processing.py
+++ Pre_Processing.py
@@ -1,28 +1,39 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 
-cs=pd.read_csv('Data.csv')
-x=cs.iloc[:,:-1].values
-y=cs.iloc[:,-1].values
+csv=pd.read_csv('Data.csv')
+x=csv.iloc[:,:-1].values
+y=csv.iloc[:,-1].values
 
 from sklearn.impute import SimpleImputer
-si=SimpleImputer(missing_values=np.nan,strategy="median")
+si=SimpleImputer(missing_values=np.nan,strategy='median')
 si.fit(x[:,1:3])
 x[:,1:3]=si.transform(x[:,1:3])
 
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
-ct=ColumnTransformer(transformers=[("encoder",OneHotEncoder(),[0])],remainder='passthrough')
+ct=ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[0])],remainder='passthrough')
 x=np.array(ct.fit_transform(x))
 
 from sklearn.preprocessing import LabelEncoder
 le=LabelEncoder()
-y=le.fit_transform(y) 
+y=le.fit_transform(y)
+
 from sklearn.model_selection import train_test_split
-atr,ate,btr,bte=train_test_split(x,y,test_size=0.3,random_state=1)
+xtr,xts,ytr,yts=train_test_split(x,y,test_size=0.2,random_state=1)
 
 from sklearn.preprocessing import StandardScaler
 sc=StandardScaler()
-atr=sc.fit_transform(atr)
-ate=sc.transform(ate)
+xtr=sc.fit_transform(xtr)
+xts=sc.transform(xts)
+
+from sklearn.linear_model import LinearRegression                #Extra Part for simple Linear Regression starts here.
+lr=LinearRegression()
+lr.fit(xtr,ytr)
+
+yp=lr.predict(xts)
+
+from sklearn.metrics import r2_score
+print(r2_score(yts, yp))                                         ##Extra Part for simple Linear Regression ends here.
+
